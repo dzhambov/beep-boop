ff1ffce90b6f579a0b1825d1c4c5d55bcf98107e
Student <example@example.com>
2019-11-01 13:34:35 -0700

    Add display user result.

--- js/scripts.js
+++ js/scripts.js
@@ -5,22 +5,23 @@ $(document).ready(function(){
     event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = beepBoop(number);
-    console.log();
+    // console.log();
     $("#result").text(result);
   });
-
 });
+
+
 // Business logic
 var beepBoop = function (number) {
-
   for (var i = 0; i <= number; i++);
-    if (i % 3 === 0) {
+
+    if (i === 3) {
     return ("Sorry, You got Busted!");
 
-  } else if (i % 2 === 0) {
+  } else if (i === 2) {
     return ("Boop!");
 
-  } else if (i % 1 === 0) {
+  } else if (i === 1) {
     return ("Beep");
 
   }
