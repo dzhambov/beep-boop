38a093eaa843b312f1d21153fa8e170702189b01
Student <example@example.com>
2019-11-01 11:40:44 -0700

    Add function to capture user's input.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,33 @@
+
 // User interface logic
 $(document).ready(function(){
   $("#user-number").submit(function(event){
     event.preventDefault();
-    var input = $("input#number").val();
+    var number = parseInt($("input#number").val());
+    var result = beepBoop(number);
     console.log();
-    $("#output").append(result);
-  })
+    $("#result").text(result);
+  });
 
 });
-
+// Business logic
+var beepBoop = function (number) {
 function beepBoop (number) {
-  result = [];
-  for (var i = 0; i <=number; i++) {
-    if (i % 3 === 0) {
-    result.push ("Sorry, You got Busted!");
-
-  } else if (i % 2 === 0) {
-    result.push ("Boop!");
-
-  } else if (i % 1 === 0) {
-    result.push ("Beep");
-  }
+//   result = [];
+//   number = parseInt(number);
+//
+//
+//   for (var i = 0; i <=number; i++);
+//     if (i % 3 === 0) {
+//     result.push ("Sorry, You got Busted!");
+//
+//   } else if (i % 2 === 0) {
+//     result.push ("Boop!");
+//
+//   } else if (i % 1 === 0) {
+//     result.push ("Beep");
+//
+//   }
+// };
 };
-return result;
-
 };
