c9172b01f3b941991665e870962d03fdb08a093c
Student <example@example.com>
2019-11-01 10:38:51 -0700

    Add work on user interface logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
+// User interface logic
 $(document).ready(function(){
-  $("#beepBoop-game").submit(function(event){
+  $("#user-number").submit(function(event){
     event.preventDefault();
-    var input = parseInt($("input#game")
-
+    var input = $("input#number").val();
+    console.log();
+    $("#output").append(result);
   })
 
 });
+
+function beepBoop (number) {
+  result = [];
+  for (var i = 0; i <=number; i++) {
+    if (i % 3 === 0) {
+    result.push ("Sorry, You got Busted!");
+
+  } else if (i % 2 === 0) {
+    result.push ("Boop!");
+
+  } else if (i % 1 === 0) {
+    result.push ("Beep");
+  }
+};
+return result;
+
+};
