ceef2324ff0596c35238182cb2819362ffa15037
Hristo Dzhambov <dzhambov@hotmail.com>
2019-11-11 15:43:28 -0800

    Add numbers to display.

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     event.preventDefault();
     var number = parseInt($("input#number").val());
     var result = beepBoop(number);
-    console.log();
+    // console.log();
     $("#result").text(result);
   });
 });
@@ -15,15 +15,15 @@ $(document).ready(function() {
 var beepBoop = function (number)  {
   var arr = [];
   for (var i = 0; i <= number; i++) {
-
     if (i.toString().includes("3")) {
       arr.push("Sorry, You got Busted!");
-      } else if (i.toString().includes("2")) {
-        arr.push("I say Boop!");
-      } else if (i.toString().includes("1")) {
-        arr.push("You say Beep!");
-
-      }
+    } else if (i.toString().includes("2")) {
+      arr.push("I say Boop!");
+    } else if (i.toString().includes("1")) {
+      arr.push("You say Beep!");
+    } else {
+      arr.push(i);
+    }
   }
   return arr;
 };
